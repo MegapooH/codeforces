b23eb5045c4b410147f8310a650a1efe2c6a03ca
MegapooH <misha.muruev@gmail.com>
2018-11-24 17:30:52 +0800

    Не решена еще

---
+++ r1093D.py
@@ -0,0 +1,18 @@
+def solve():
+    line = input()
+    n = int(line.split(' ')[0])
+    l = int(line.split(' ')[1])
+    r = int(line.split(' ')[2])
+    k = int(line.split(' ')[3])
+    maxn = 2 * n
+    rslt = -1
+    while maxn > n:
+        if True:
+            rslt = max - n
+            break
+        else:
+            maxn -= 1
+
+    print(rslt)
+
+solve()
